7d96c4298dc6b45a787f8827673f8d15eedcc622
Irene Lee <86876293+juwonleedev@users.noreply.github.com>
2023-01-25 17:35:59 +0900

    #1.9 Array and slice

--- main.go
+++ main.go
@@ -5,17 +5,14 @@ import (
 )
 
 func main () {
-	a:=2
-	b:=a // 값이 복사됨 
-	a=10
-	c:=&a
-	// & -> 메모리 주소로 안내
-	// * -> 메모리가 가리키는 값을 안내 (주소에 담긴 값)
-	fmt.Println(a,b,c,*c)
-	// 10, 2, 메모리값, 10
-	*c = 20
-	// *를 사용해서 a 값을 업데이트 
-	// *주소 = 없데이트할 값 
-	fmt.Println(a,b,c,*c)
-	// 20 2 0xc0000160b8 20
+	// Arrays 
+	names := [5]string{"nico","lynn","dal"}
+	names[3] = "alalal"
+	names[4] = "fdfdfd"
+
+	//Slices = arrays without length 
+	names2 := []string{"nico", "lynn","dal"}
+	names2= append(names2,"irene")
+	// append (slice, 추가할 값) 을 이용해 slice에 값 추가
+	fmt.Println(names, names2)
 }
