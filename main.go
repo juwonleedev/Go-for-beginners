007c9c11f2dfade22dabe6c3270663e82e473bba
Irene Lee <86876293+juwonleedev@users.noreply.github.com>
2023-01-25 16:58:08 +0900

    #1.4 Function pt.1

--- main.go
+++ main.go
@@ -1,20 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // 작성할 패키지 명
+func multiply(a int,b int) int {
+	return a*b
+	// 필수적으로 타입 명시
+}
+
+func multiply2(a, b int) int{
+	return a*b
+}
+
+// go는 리턴값을 여러개 반환 가능 
+func lenAndUpper(name string) (int,string){
+	return len(name),strings.ToUpper(name)
+	// go는 표준 라이브러리가 잘 되어 있다
+}
+
+func repeatMe(words ...string){
+	fmt.Println(words)
+}
 
 func main() {
-	const name string = "nico"
-	// go 언어는 type 언어여서 타입 명시
+	fmt.Println(multiply(2,2))
 	
-	var name2 string = "nico"
-	name2="lynn"
-	fmt.Println(name2)
+	totalLength, upperName := lenAndUpper("nico")
+	fmt.Println(totalLength,upperName)
+	// go 는 사용되지 않는 변수에 대해 에러 발생
+	// totalLength, _ := lenAndUppper("nico")로 리턴값 무시 가능 
 
-	// 간편 작성 : go가 type 추론 후 추론한 타입에서 변경은 불가 
-	name3:="nico"
-	//name3:=false 이렇게 수정 불가 
-	// 축약형: func 안에서만 + 변수에 한정해서 가능 
-	fmt.Println(name3)
+	repeatMe("nico","love","cute")
 }
