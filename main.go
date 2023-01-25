a997b233ad73eed82998742b83339fec49daf4f4
Irene Lee <86876293+juwonleedev@users.noreply.github.com>
2023-01-25 15:47:40 +0900

    #1.packages and imports

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main 
 // 작성할 패키지 명 
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/juwonleedev/learngo/something"
+)
 
 func main() {
 	// main fun - entry point 
 	fmt.Println("Hellow World!")
+	something.SayHello()
 }
